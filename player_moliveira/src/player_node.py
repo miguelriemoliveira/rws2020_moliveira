a6889ce055ae96da69bbd55c99a223a903a3ded4
Miguel Armando Riem Oliveira <m.riem.oliveira@gmail.com>
2020-02-20 11:22:58 +0000

    Reading team parameters.

--- player_moliveira/src/player_node.py
+++ player_moliveira/src/player_node.py
@@ -1,7 +1,38 @@
 #!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+
+class Player:
+
+    def __init__(self, player_name):
+        self.player_name = player_name
+
+        rospy.logwarn('I am ' + self.player_name)
+
+        red_team = rospy.get_param('/red_team')
+        green_team = rospy.get_param('/green_team')
+        blue_team = rospy.get_param('/blue_team')
+
+        print('red_team = ' + str(red_team))
+        print('green_team = ' + str(green_team))
+        print('blue_team = ' + str(blue_team))
+
+
+def callback(msg):
+    print("Recevied a message containing string " + msg.data)
+
 
 def main():
     print("Hello player node!")
+
+    rospy.init_node('moliveira', anonymous=False)
+
+    player = Player('moliveira')
+
+    rospy.Subscriber("chatter", String, callback)
+    rospy.spin()
+
 
 if __name__ == "__main__":
     main()
