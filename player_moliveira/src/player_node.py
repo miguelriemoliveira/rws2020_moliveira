d318c63b893448c50c0602c2eed8810124e55dca
Miguel Armando Riem Oliveira <m.riem.oliveira@gmail.com>
2020-02-20 11:57:49 +0000

    Reading team parameters.

--- player_moliveira/src/player_node.py
+++ player_moliveira/src/player_node.py
@@ -8,7 +8,6 @@ class Player:
     def __init__(self, player_name):
         self.player_name = player_name
 
-        rospy.logwarn('I am ' + self.player_name)
 
         red_team = rospy.get_param('/red_team')
         green_team = rospy.get_param('/green_team')
@@ -18,6 +17,24 @@ class Player:
         print('green_team = ' + str(green_team))
         print('blue_team = ' + str(blue_team))
 
+        if self.player_name in red_team:
+            self.my_team, self.prey_team, self.hunter_team = 'red', 'green', 'blue'
+            self.my_players, self.preys, self.hunters = red_team, green_team, blue_team
+
+        elif self.player_name in green_team:
+            self.my_team, self.prey_team, self.hunter_team = 'green', 'blue', 'red'
+            self.my_players, self.preys, self.hunters = green_team, blue_team, red_team
+
+        elif self.player_name in blue_team:
+            self.my_team, self.prey_team, self.hunter_team =  'blue', 'red', 'green'
+            self.my_players, self.preys, self.hunters =  blue_team, red_team, green_team
+
+        else:
+            rospy.logerr('My name is not in any team. I want to play!')
+            exit(0)
+
+
+        rospy.logwarn('I am ' + self.player_name + ' and I am on team ' + self.my_team + '. ' + self.prey_team + ' players are all going die!')
 
 def callback(msg):
     print("Recevied a message containing string " + msg.data)
